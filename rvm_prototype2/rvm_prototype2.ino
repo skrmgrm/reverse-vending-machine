0002143594148a47a833231e3dc01617cb3c2aca
Joshua Barlolong <86538650+skrmgrm@users.noreply.github.com>
2022-04-30 16:22:55 +0800

    Add files via upload

--- rvm_prototype2/rvm_prototype2.ino
+++ rvm_prototype2/rvm_prototype2.ino
@@ -9,7 +9,11 @@
 #define redLed 8
 #define greenLed 9
 
-int bottleNumbers = 0;
+int bottleNumber = 0;    // variable to store the servo position
+
+int buttonPin = 6;
+int buttonState = 0;
+int previousButtonState = 1;
 
 Servo entranceServo;
 Servo sorterServo;
@@ -19,8 +23,10 @@ HX711 scale;
 
 void setup() {
   // put your setup code here, to run once:
-
+//  Serial.begin(9600);
   servoSetup();
+  pinMode(buttonPin, INPUT_PULLUP);
+
   pinMode(redLed, OUTPUT);
   pinMode(greenLed, OUTPUT);
 
@@ -34,14 +40,17 @@ void loop() {
   // put your main code here, to run repeatedly:
   static char serial;
 
+  buttonState = digitalRead(buttonPin);
+
   Serial.print("Reading: ");
 
   float weight = scale.get_units();
   weight = DecimalRound(weight, 1);
-  Serial.print(weight);  //scale.get_units() returns a float
-  Serial.print(" g");    //You can change this to kg but you'll need to refactor the calibration_factor
-  Serial.println();
-
+//  Serial.print(weight);  //scale.get_units() returns a float
+//  Serial.print(" g");    //You can change this to kg but you'll need to refactor the calibration_factor
+//  Serial.println();
+  digitalWrite(greenLed, LOW);
+  delay(100);
   digitalWrite(greenLed, HIGH);
 
   if (weight > 0) {
@@ -51,17 +60,42 @@ void loop() {
       if (Serial.available()) {
         serial = Serial.read();
 
-        if (serial == '2') {
+        if (serial == '1') {
+          bottleNumber = bottleNumber + 1;
           objectAccepted();
-        } else if (serial == '1') {
+        } else if (serial == '2') {
           objectRejected();
         }
       }
     }
     else if ( weight > 3.00)  {
+      Serial.begin(9600);
       Serial.println("Object Rejected");
       objectRejected();
     }
+  }
+
+  if (buttonState == 1 && previousButtonState == 0) {
+    Serial.println("button clicked");
+    dispenseCoins();
+    bottleNumber = 0;
+  }
+
+  previousButtonState = buttonState;
+//
+//  Serial.print("current state: ");
+//  Serial.println(buttonState);
+//  Serial.print("previous state: ");
+//  Serial.println(previousButtonState);
+
+}
+
+void dispenseCoins () {
+  for (int i = 0; i < bottleNumber; i++) {
+    dispenserServo.write(0);
+    delay(500);
+    dispenserServo.write(80);
+    delay(500);
   }
 }
 
@@ -74,14 +108,13 @@ float DecimalRound(float input, int decimals)
 void objectAccepted() {
   Serial.println("Object Accepted");
   digitalWrite(greenLed, LOW);
-  bottleNumbers += 1;
   entranceServo.write(150);  // sets the servo position according to the scaled value
   delay(2000);
   sorterServo.write(150);
   delay(2000);
   sorterServo.write(75);
   delay(2000);
-  entranceServo.write(65);  // sets the servo position according to the scaled value
+  entranceServo.write(60);  // sets the servo position according to the scaled value
   delay(1500);
   digitalWrite(greenLed, HIGH);
   Serial.end();
@@ -92,23 +125,23 @@ void objectRejected() {
   digitalWrite(redLed, HIGH);
   entranceServo.write(150);  // sets the servo position according to the scaled value
   delay(2000);
+  digitalWrite(redLed, LOW);
   sorterServo.write(20);
   delay(2000);
   sorterServo.write(75);
   delay(2000);
-  entranceServo.write(65);  // sets the servo position according to the scaled value
+  entranceServo.write(60);  // sets the servo position according to the scaled value
   delay(1500);
-  digitalWrite(redLed, LOW);
   Serial.end();
 }
 
 void servoSetup() {
   entranceServo.attach(24);
   sorterServo.attach(22);
-  dispenserServo.attach(23);
+  dispenserServo.attach(26);
   delay(1000);
   sorterServo.write(75);
   delay(1000);
-  entranceServo.write(65);
+  entranceServo.write(60);
   delay(1000);
 }
